70329ecdb5eb22b8b41dd52049080394ccb5eda7
guigarciaa <guigarcia.a@outlook.com>
2022-10-30 08:50:16 -0300

    add title component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AnchorComponent } from "./atoms/anchor/anchor.component";
-import { NavbarComponent } from "./molecules/navbar/navbar.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AnchorComponent } from './atoms/anchor/anchor.component';
+import { TitleComponent } from './atoms/title/title.component';
+import { NavbarComponent } from './molecules/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AnchorComponent, NavbarComponent],
+  declarations: [AnchorComponent, NavbarComponent, TitleComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [],
-  exports: [AnchorComponent, NavbarComponent],
+  exports: [AnchorComponent, NavbarComponent, TitleComponent],
 })
 export class ComponentsModule {}
