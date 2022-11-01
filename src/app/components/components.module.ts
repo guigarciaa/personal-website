c9b8286fd4476b34ded9ef6b358da03bc8e45342
guigarciaa <guigarcia.a@outlook.com>
2022-11-01 08:11:42 -0300

    feat: title and sub title section

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,12 +4,26 @@ import { AnchorComponent } from './atoms/anchor/anchor.component';
 import { TitleComponent } from './atoms/title/title.component';
 import { NavbarComponent } from './organisms/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SubTitleComponent } from './atoms/sub-title/sub-title.component';
+import { TitleSectionComponent } from './molecules/title-section/title-section.component';
 
 @NgModule({
-  declarations: [AnchorComponent, NavbarComponent, TitleComponent],
+  declarations: [
+    AnchorComponent,
+    NavbarComponent,
+    TitleComponent,
+    SubTitleComponent,
+    TitleSectionComponent,
+  ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [],
-  exports: [AnchorComponent, NavbarComponent, TitleComponent],
+  exports: [
+    AnchorComponent,
+    NavbarComponent,
+    TitleComponent,
+    SubTitleComponent,
+    TitleSectionComponent,
+  ],
 })
 export class ComponentsModule {}
