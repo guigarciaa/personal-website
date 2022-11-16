2b637234b950f8c126a20ff40f85199525aa51f3
guigarciaa <guigarcia.a@outlook.com>
2022-11-16 10:09:13 -0300

    feat: home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ComponentsModule } from "./components/components.module";
+import { HomepageComponent } from "./pages/homepage/homepage.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent
+],
   imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
